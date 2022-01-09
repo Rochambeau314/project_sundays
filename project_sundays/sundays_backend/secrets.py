0e4a3aecdfc2170b1d1293f7a15226bb597643fb
Rochambeau314 <71663254+Rochambeau314@users.noreply.github.com>
2022-01-08 20:08:42 -0600

    Delete secrets.py

--- project_sundays/sundays_backend/secrets.py
+++
@@ -1,3 +0,0 @@
-client_id = '956682115584-1od101difchaua00q2oimmnp4kurc0vu.apps.googleusercontent.com'
-client_secret = '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'
-SECRET_KEY = 'django-insecure-kk^fptguflhj74oy5pjlwbo8(s-xvi30n3qfpx73+s-ww@!d2_'
